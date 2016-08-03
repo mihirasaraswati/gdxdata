1179f8002861fe05470b5270759cc0612dcabbfb
mihiriyer <knowledge@jangal.info>
2016-08-03 15:02:25 -0400

    separated data prep program into one for state and the other for counties.
    added reading of FY09 to FY07. accounted for GU not being in FY07 and FY08

---
+++ Code_GDX_Explore_DataPrep_State.R
@@ -0,0 +1,70 @@
+#ATTENTION - this command should be run before loading xlsx library
+options(java.parameters = "-Xmx8000m")
+
+library(xlsx)
+library(dplyr)
+
+# Data Read & Consolidate -------------------------------------------------
+
+#create empty dataframe to store data from xl files
+gdxstate0715 <- data.frame()
+#setup vector of column classes
+gdxclasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+
+#read files that end with xlsx
+gdxfiles = list.files(".", pattern="GDX_FY[0-1][0-9].xlsx", all.files = FALSE, full.names = FALSE)
+
+#prob - associating fys with data from files names. there are more than 3 variations of fys for 17 files. easier to rename the files manually
+fys <- c(2007:2015)
+
+#NOTE will read up to 2009, the remaining files differ. GU was added in 2009 and Uniques weren't reported in 2006. 
+for(i in 1:length(gdxfiles)){
+  tmpdfState <-  read.xlsx(gdxfiles[i],
+                           sheetIndex = 1,
+                           colIndex = c(1:11),
+                           header=FALSE,
+                           startRow = 6,
+                           endRow = 58,
+                           stringsAsFactors = FALSE)
+  
+  #remove white spaces in State Column
+  tmpdfState$X1 <- trimws(tmpdfState$X1)
+  
+  #create a column to store fiscal year; number of rows should match those of the dataframe
+  fy <- rep(fys[i],nrow(tmpdfState))
+  
+  # attach fiscal year to the temporary dataframe - this represents all states for a fiscal year
+  tmpdfState <- cbind(fy, tmpdfState, stringsAsFactors = FALSE)
+  
+  gdxstate0715 <- rbind(gdxstate0715, tmpdfState, stringsAsFactors = FALSE)
+  
+}
+
+#add shortened column names
+names(gdxstate0715) <- c("FY", "StateName", "VetPop", "TotX", "CP", "Cons", "EduVoc", "Loan", "GOE", "InsInd", "MedCare", "Uniques")
+
+#convert FY to character
+gdxstate0715$FY <- as.character(gdxstate0715$FY)
+
+#convert to numeric
+gdxstate0715$Uniques <- as.numeric(gdxstate0715$Uniques)
+
+#add state abbreviations
+st <- data.frame(state.abb, state.name, stringsAsFactors = FALSE)
+st <- rbind(st,c("DC", "District of Columbia"))
+st <- rbind(st,c("PR", "Puerto Rico"))
+st <- rbind(st,c("GU", "Guam"))
+
+gdxstate0715$State <- st$state.abb[match(gdxstate0715$State, st$state.name)]
+
+#drop NA rows
+gdxstate0715 <- filter(gdxstate0715,!is.na(State))
+
+#reorder columns
+gdxstate0715 <- gdxstate0715[, c(1,13,2:12)]
+
+# Remove temp variables
+rm( tmpdfState, fy, fys, gdxclasses, gdxfiles, i, st)
+#SAVE State data
+saveRDS(gdxstate0715, file="gdxstate0715.rds")
+
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # gdxdata
 R program to consolidate VA's Geographic Expenditure data from FY 2007 to 2015
+
+
+File Name Changes:
+1. GX_FY12_V1.xlsx TO GDX_FY12
+2. GDX_FY10V14 to GDX_FY10
+3. GDX_FY06_V090409 
+4. GDX_FY07_V090401
+5. GDX_FY08_V2
+6. GDX_FY09_2
+
+
+Workbook Setup:
+
+1. 55 sheets total
+2. 1 X State Level Expenditure
+3. 50 X States
+4. 1 X DC, 1 X PR, 1 X GU
+5. 1 X Data Description
+
+
+State Level Expenditure:
+Start: Row 6
+End: Row 58
+
+State Sheets:
+Start: Row 4:
+End: Variable (first blank)
+
+Used read.xlsx - formulae weren't being evaluated with readRows or xlsx2. Runtime 6:56 
+
+Ubunutu need to libgeo-dev for rgeos package and then libgdal.dev for rgdal package
+
+read.xlsx2 - runtime of over 10min, terminated without completion.
+
+
+County Names that need to be corrected to match FIPS names:
+1. All of Alaska
+2. AR Saint Francis County (st. francis county)
+3. DC 
+4. LA
+5. all the counties that start with "Mc" some with St. 
+
+Remove AS, VI and ___ from the Census county file
+
+converted all the xls to xlsx
+
+fy07 Hawaii in tab was written as H1 manually changed to HI
+
+fy09 PR tab starts at column D and not A.52 because 53rd is guam which is not present in FY08 & FY07
